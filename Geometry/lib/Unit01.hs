bcc2b4aaaa0067e069dde78773e151da0242e83d
Fernando Alegre <git@fernando.alemor.org>
2015-09-08 12:56:54 -0500

    .

--- Geometry/lib/Unit01.hs
+++ Geometry/lib/Unit01.hs
@@ -1,5 +1,5 @@
 
-module Unit01(projection,collinear,dist,(=~)) where
+module Unit01(projection,collinear,dist,ushape,(=~)) where
 
 import qualified Drawing as D
 import Geometry
@@ -33,3 +33,16 @@ collinear points = map (projection (a,b)) rest
           rest = drop 2 points
 
 x =~ y = abs (x-y) < 0.01
+
+ushape points =
+    [(p1,q1),(p1,q2),(p2,q2),(p2,q3),
+     (p3,q3),(p3,q4),(p4,q4),(p4,q1)]
+    where [(x1,y1),(x2,y2),(x3,y3),(x4,y4)] = take 4 points
+          p1 = 0.5*x1 - 7.5
+          q1 = 0.5*y1 - 7.5
+          p2 = 0.5*x2 + 7.5
+          q2 = 0.5*y2 + 7.5
+          p3 = 0.5*x3 + 2.5
+          q3 = 0.5*y3 - 2.5
+          p4 = 0.5*x4 - 2.5
+          q4 = 0.5*y4 + 2.5
